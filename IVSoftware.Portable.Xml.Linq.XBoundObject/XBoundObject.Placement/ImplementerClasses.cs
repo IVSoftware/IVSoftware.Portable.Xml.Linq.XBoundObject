d4d2e30ecf9e9508322a2f3e4d467e63b2916d8b
Tom Gregor <tgregor@ivsoftware.com>
2025-03-27 10:52:06 -0600

    ORIGIN XBoundObjectImplementer

---
+++ IVSoftware.Portable.Xml.Linq.XBoundObject/XBoundObject.Placement/ImplementerClasses.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Text;
+using System.Xml.Linq;
+
+namespace IVSoftware.Portable.Xml.Linq.XBoundObject.Placement
+{
+    public class XBoundObjectImplementer : IXBoundObject
+    {
+        public XBoundObjectImplementer() { }
+        public XBoundObjectImplementer(XElement xel) => _xel = xel;
+        public XElement XEL => _xel;
+        private XElement _xel;
+        public XElement InitXEL(XElement xel)
+        {
+            if(_xel is null)
+            {
+                _xel = xel;
+            }
+            else
+            {
+                if(!ReferenceEquals(xel, _xel))
+                {
+                    throw new InvalidOperationException($"{nameof(XEL)} is already initialized to a diiferent value.");
+                }
+            }
+            return _xel;
+        }
+    }
+    public class XBoundViewObjectImplementer : XBoundObjectImplementer, IXBoundViewObject
+    {
+        public XElement XEL => throw new NotImplementedException();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ExpandedState Collapse(string path, Enum pathAttribute = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ExpandedState Expand(string path, Enum pathAttribute = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public XElement InitXEL(XElement xel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public XElement Show(string path, Enum pathAttribute = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
