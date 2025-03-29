1024119c8d0084def28f53f2d90287da94e60473
Tom Gregor <tgregor@ivsoftware.com>
2025-03-28 22:31:51 -0600

    SFSG UTAC @ 25 B4 Expand

--- MSTestProject/TestClass_XBoundViewObject.cs
+++ MSTestProject/TestClass_XBoundViewObject.cs
@@ -18,16 +18,25 @@ public class TestClass_XBoundViewObject
         var xroot = new XElement("root").UseXBoundView(items, 2);
         var context = xroot.To<ViewContext>(@throw: true);
 
-        // WinOS path in a WinOS test.
-        path =
-            @"C:\Github\IVSoftware\Demo\IVSoftware.Demo.CrossPlatform.FilesAndFolders\BasicPlacement.Maui\BasicPlacement.Maui.csproj";
-        xroot.Show(path);
+        subtestShowPathSimpleThenSyncList();
+        subtestCollapseDemoNode();
+        subtestAddNotVisibleFloppyB();
+        subtestMakeBDriveVisible();
 
-        actual = xroot.SortAttributes<StdAttributeNameXBoundViewObject>().ToString();
-        actual.ToClipboard();
-        actual.ToClipboardExpected();
-        { }
-        expected = @" 
+        #region S U B T E S T S
+
+        void subtestShowPathSimpleThenSyncList()
+        {
+            // WinOS path in a WinOS test.
+            path =
+                @"C:\Github\IVSoftware\Demo\IVSoftware.Demo.CrossPlatform.FilesAndFolders\BasicPlacement.Maui\BasicPlacement.Maui.csproj";
+            xroot.Show(path);
+
+            actual = xroot.SortAttributes<StdAttributeNameXBoundViewObject>().ToString();
+            actual.ToClipboard();
+            actual.ToClipboardExpected();
+            { }
+            expected = @" 
 <root viewcontext=""[ViewContext]"">
   <xnode text=""C:"" isvisible=""True"" plusminus=""Expanded"" datamodel=""[Item]"">
     <xnode text=""Github"" isvisible=""True"" plusminus=""Expanded"" datamodel=""[Item]"">
@@ -43,25 +52,24 @@ public class TestClass_XBoundViewObject
     </xnode>
   </xnode>
 </root>"
-        ;
+            ;
 
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting an expanded tree with XBO='Item' culminating in a leaf node."
+            );
 
-        Assert.AreEqual(
-            expected.NormalizeResult(),
-            actual.NormalizeResult(),
-            "Expecting an expanded tree with XBO='Item' culminating in a leaf node."
-        );
+            context.SyncList();
+            joined =
+                string
+                .Join(
+                    Environment.NewLine,
+                    items.Select(_ =>
+                        context.GetIndentedText(_.XEL)));
+            actual = joined;
 
-        context.SyncList();
-        joined =
-            string
-            .Join(
-                Environment.NewLine,
-                items.Select(_ =>
-                    context.GetIndentedText(_.XEL)));
-        actual = joined;
-
-        expected = @" 
+            expected = @" 
 C:
   Github
     IVSoftware
@@ -69,19 +77,22 @@ C:
         IVSoftware.Demo.CrossPlatform.FilesAndFolders
           BasicPlacement.Maui
             BasicPlacement.Maui.csproj"
-        ;
+            ;
 
-        Assert.AreEqual(
-            expected.NormalizeResult(),
-            actual.NormalizeResult(),
-            "Expecting indented monospace text."
-        );
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting indented monospace text."
+            );
+        }
 
-        path = Path.Combine("C:", "Github", "IVSoftware", "Demo");
-        xroot.Collapse(path);
+        void subtestCollapseDemoNode()
+        {
+            path = Path.Combine("C:", "Github", "IVSoftware", "Demo");
+            xroot.Collapse(path);
 
-        actual = xroot.SortAttributes<StdAttributeNameXBoundViewObject>().ToString();
-        expected = @"
+            actual = xroot.SortAttributes<StdAttributeNameXBoundViewObject>().ToString();
+            expected = @"
 <root viewcontext=""[ViewContext]"">
   <xnode text=""C:"" isvisible=""True"" plusminus=""Expanded"" datamodel=""[Item]"">
     <xnode text=""Github"" isvisible=""True"" plusminus=""Expanded"" datamodel=""[Item]"">
@@ -97,40 +108,45 @@ C:
     </xnode>
   </xnode>
 </root>"
-        ;
+            ;
 
-        Assert.AreEqual(
-            expected.NormalizeResult(),
-            actual.NormalizeResult(),
-            "Expecting removal of all isvisible and plusminus attrs below the collapsed node."
-        );
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting removal of all isvisible and plusminus attrs below the collapsed node."
+            );
 
-        context.SyncList();
-        joined =
-            string
-            .Join(
-                Environment.NewLine,
-                items.Select(_ =>
-                    context.GetIndentedText(_.XEL)));
-        actual = joined;
-        expected = @" 
+            // Apps may put this on a WDT...
+            context.SyncList();
+            joined =
+                string
+                .Join(
+                    Environment.NewLine,
+                    items.Select(_ =>
+                        context.GetIndentedText(_.XEL)));
+            actual = joined;
+            expected = @" 
 C:
   Github
     IVSoftware
       Demo";
 
-        Assert.AreEqual(
-            expected.NormalizeResult(),
-            actual.NormalizeResult(),
-            "Expecting indented monospace text."
-        );
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting indented monospace text."
+            );
+        }
 
-        path = Path.Combine("B:(Floppy Disk)");
-        item = xroot.FindOrCreate<Item>(path);
-        Assert.IsTrue(item is Item, $"Expecting adhoc not-visible {nameof(Item)}");
+        void subtestAddNotVisibleFloppyB()
+        {
 
-        actual = xroot.SortAttributes<StdAttributeNameXBoundViewObject>().ToString();
-        expected = @" 
+            path = Path.Combine("B:(Floppy Disk)");
+            item = xroot.FindOrCreate<Item>(path);
+            Assert.IsTrue(item is Item, $"Expecting adhoc not-visible {nameof(Item)}");
+
+            actual = xroot.SortAttributes<StdAttributeNameXBoundViewObject>().ToString();
+            expected = @" 
 <root viewcontext=""[ViewContext]"">
   <xnode text=""C:"" isvisible=""True"" plusminus=""Expanded"" datamodel=""[Item]"">
     <xnode text=""Github"" isvisible=""True"" plusminus=""Expanded"" datamodel=""[Item]"">
@@ -148,16 +164,19 @@ C:
   <xnode text=""B:(Floppy Disk)"" item=""[Item]"" />
 </root>";
 
-        Assert.AreEqual(
-            expected.NormalizeResult(),
-            actual.NormalizeResult(),
-            "Expecting xroot UNSORTED with item not visible."
-        );
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting xroot UNSORTED with item not visible."
+            );
+        }
 
-        item.IsVisible = true;
+        void subtestMakeBDriveVisible()
+        {
+            item.IsVisible = true;
 
-        actual = xroot.SortAttributes<StdAttributeNameXBoundViewObject>().ToString();
-        expected = @" 
+            actual = xroot.SortAttributes<StdAttributeNameXBoundViewObject>().ToString();
+            expected = @" 
 <root viewcontext=""[ViewContext]"">
   <xnode text=""C:"" isvisible=""True"" plusminus=""Expanded"" datamodel=""[Item]"">
     <xnode text=""Github"" isvisible=""True"" plusminus=""Expanded"" datamodel=""[Item]"">
@@ -175,21 +194,21 @@ C:
   <xnode text=""B:(Floppy Disk)"" isvisible=""True"" item=""[Item]"" />
 </root>";
 
-        Assert.AreEqual(
-            expected.NormalizeResult(),
-            actual.NormalizeResult(),
-            "Expecting xroot unsorted with item VISIBLE."
-        );
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting xroot unsorted with item VISIBLE."
+            );
 
-        xroot.Sort();
+            xroot.Sort();
 
 
-        actual = xroot.SortAttributes<StdAttributeNameXBoundViewObject>().ToString();
+            actual = xroot.SortAttributes<StdAttributeNameXBoundViewObject>().ToString();
 
-        actual.ToClipboard();
-        actual.ToClipboardExpected();
-        { }
-        expected = @" 
+            actual.ToClipboard();
+            actual.ToClipboardExpected();
+            { }
+            expected = @" 
 <root viewcontext=""[ViewContext]"">
   <xnode text=""B:(Floppy Disk)"" isvisible=""True"" item=""[Item]"" />
   <xnode text=""C:"" isvisible=""True"" plusminus=""Expanded"" datamodel=""[Item]"">
@@ -206,37 +225,40 @@ C:
     </xnode>
   </xnode>
 </root>"
-        ;
+            ;
 
-        Assert.AreEqual(
-            expected.NormalizeResult(),
-            actual.NormalizeResult(),
-            "Expecting xroot SORTED with item visible."
-        );
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting xroot SORTED with item visible."
+            );
 
 
-        context.SyncList();
-        joined =
-            string
-            .Join(
-                Environment.NewLine,
-                items.Select(_ =>
-                    context.GetIndentedText(_.XEL)));
-        actual = joined;
-        actual.ToClipboard();
-        actual.ToClipboardAssert("Expecting B: is added, but NOT SORTED.");
-        { }
-        expected = @" 
+            context.SyncList();
+            joined =
+                string
+                .Join(
+                    Environment.NewLine,
+                    items.Select(_ =>
+                        context.GetIndentedText(_.XEL)));
+            actual = joined;
+            actual.ToClipboard();
+            actual.ToClipboardExpected();
+            { }
+            expected = @" 
+B:(Floppy Disk)
 C:
   Github
     IVSoftware
       Demo";
 
-        Assert.AreEqual(
-            expected.NormalizeResult(),
-            actual.NormalizeResult(),
-            "Expecting B: is added, but NOT SORTED."
-        );
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting B: is visible and sorted alphabetically."
+            );
+        }
+        #endregion S U B T E S T S
     }
 
     /// <summary>
