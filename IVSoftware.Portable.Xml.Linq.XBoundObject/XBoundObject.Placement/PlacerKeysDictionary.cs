64b189ee5f10de8248a8eea3d0736acd86be5e22
Tom Gregor <tgregor@ivsoftware.com>
2026-05-02 12:39:44 -0600

    NuGet Release 2.1.0-rc

--- IVSoftware.Portable.Xml.Linq.XBoundObject/XBoundObject.Placement/PlacerKeysDictionary.cs
+++ IVSoftware.Portable.Xml.Linq.XBoundObject/XBoundObject.Placement/PlacerKeysDictionary.cs
@@ -1,8 +1,28 @@
-﻿using System;
+﻿using IVSoftware.Portable.Common.Attributes;
+using System;
 using System.Collections.Generic;
-using System.Text;
-
 namespace IVSoftware.Portable.Xml.Linq.XBoundObject.Placement
 {
-    public class PlacerKeysDictionary : Dictionary<StdPlacerKeys, string> { }
+    /// <summary>
+    /// Semantic option bag for placer customization.
+    /// </summary>
+    /// <remarks>
+    /// Includes 3 inert signatures for the express purpose of
+    /// fulfilling an implicit public contract on the BC.
+    /// </remarks>
+    public class PlacerKeysDictionary : Dictionary<StdPlacerKeys, string>
+    {
+        [Careful("Contract fulfillment on base class - do not remove.")]
+        public int Capacity => Count;
+
+        [Careful("Contract fulfillment on base class - do not remove.")]
+        public object GetAlternateLookup() => this;
+
+        [Careful("Contract fulfillment on base class - do not remove.")]
+        public bool TryGetAlternateLookup(out object lookup)
+        {
+            lookup = this;
+            return true;
+        }
+    }
 }
---
+++ MSTestProject/TestClass_PublicContract.cs
@@ -0,0 +1,90 @@
+﻿using IVSoftware.WinOS.MSTest.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XBoundObject.MSTest
+{
+    [TestClass]
+    public class TestClass_PublicContract
+    {
+        [TestMethod]
+        public void Test_GetBreakingChanges()
+        {
+            string actual, expected;
+            string baseline;
+
+
+            subtest_AssemblyOnly();
+            subtest_AssemblyAndDependencies();
+
+            #region S U B T E S T S
+            void subtest_AssemblyOnly()
+            {
+                baseline =
+                    "XBoundObject.MSTest.Witness.XBoundObject Version=2.0.3.xml"
+                    .ReadManifestResourceFile<TestClass_PublicContract>();
+                string revision =
+                    typeof(IVSoftware.Portable.Xml.Linq.XBoundAttribute)
+                    .Assembly
+                    .ToPublicContract()
+                    .ToString();
+
+                if (baseline.IsContractValid(revision, ManifestTypePolicy.AssemblyOnly))
+                {   /* G T K */
+                }
+                else
+                {
+                    var diff = baseline.GetBreakingChanges(revision, ManifestTypePolicy.AssemblyOnly);
+
+                    actual = diff!.ToString(); ;
+                    actual.ToClipboardExpected();
+                    { }
+                    expected = @" 
+<breakingChanges policy=""AssemblyOnly"" />";
+
+                    Assert.AreEqual(
+                        expected.NormalizeResult(),
+                        actual.NormalizeResult(),
+                        "Expecting no breaking changes. See diff for more."
+                    );
+                }
+            }
+            void subtest_AssemblyAndDependencies()
+            {
+                baseline =
+                    "XBoundObject.MSTest.Witness.XBoundObject Version=2.0.3.Dependencies.xml"
+                    .ReadManifestResourceFile<TestClass_PublicContract>();
+                string revision =
+                    typeof(IVSoftware.Portable.Xml.Linq.XBoundAttribute)
+                    .Assembly
+                    .ToPublicContract(ManifestTypePolicy.IVSoftwareAssembliesOnly)
+                    .ToString();
+
+                if (baseline.IsContractValid(revision, ManifestTypePolicy.IVSoftwareAssembliesOnly))
+                {   /* G T K */
+                }
+                else
+                {
+                    var diff = baseline.GetBreakingChanges(revision, ManifestTypePolicy.IVSoftwareAssembliesOnly);
+
+                    actual = diff!.ToString(); ;
+                    actual.ToClipboardExpected();
+                    { }
+                    expected = @" 
+<breakingChanges policy=""IVSoftwareAssembliesOnly"" />";
+
+                    Assert.AreEqual(
+                        expected.NormalizeResult(),
+                        actual.NormalizeResult(),
+                        "Expecting no breaking changes. See diff for more."
+                    );
+                }
+            }
+
+            #endregion S U B T E S T S
+        }
+    }
+}
